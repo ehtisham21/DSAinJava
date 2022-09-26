c17356f06ebebed198bc44f2bf0fa8249a454688
Ehtisham Butt <buttehtisham123@gmail.com>
2022-09-26 16:46:31 +0500

    Singly Linked List

--- DSAinJava/src/main/java/SinglyLinkedList.java
+++ DSAinJava/src/main/java/SinglyLinkedList.java
@@ -1,8 +1,29 @@
 
 public class SinglyLinkedList {
+    private  listnode head;
+    private static class listnode{
+        private int data;
+         listnode next;
 
+        public listnode(int data) {
+            this.data=data;
+            this.next=null;
+            
+        }
+        
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+       SinglyLinkedList list= new SinglyLinkedList();
+       list.head= new listnode(10);
+       listnode second=new listnode(20);
+       listnode third =new listnode(30);
+       listnode fourth =new listnode(40);
+       
+       //Now we will connect them together to form a chain
+       list.head.next=second;
+       second.next=third;
+       third.next=fourth;
+    
     }
     
 }
